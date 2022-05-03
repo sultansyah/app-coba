1b4af192e50a18d3595428f6176f274196222cdc
sultansyah <muhammadsultansyah14@gmail.com>
2022-05-03 12:21:02 +0700

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/sultansyah/app-coba
+
+go 1.18
+
+require github.com/sultansyah/go-coba v1.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	go_coba "github.com/sultansyah/go-coba"
+)
+
+func main() {
+	go_coba.Login("sultan")
+}
